b1ddc6fc576ad3f31cf3d284ca27f6866d3beca8
TJ512512 <160138699+TJ512512@users.noreply.github.com>
2025-03-04 16:56:49 +0000

    Update Program.cs

--- To do list/Program.cs
+++ To do list/Program.cs
@@ -16,7 +16,7 @@ namespace To_do_list
             public int date;
             public int Month;
             public int year;
-            
+
             public Task(string tName, int tImportance, int tdate, int tMonth, int tyear)
             {
                 Name = tName;
@@ -25,13 +25,19 @@ namespace To_do_list
                 Month = tMonth;
                 year = tyear;
             }
+
+            public override string ToString()
+            {
+                return $"Task Name: {Name}, Importance: {Importance}, Date: {date}/{Month}/{year}";
+            }
         }
+
         static void Main(string[] args)
         {
             List<Task> ToDoList = new List<Task>();
             InputtingList(ToDoList);
             bool AddSomething = true;
-            while (AddSomething == true)
+            while (AddSomething)
             {
                 if (ToDoList.Count == 0)
                 {
@@ -44,7 +50,7 @@ namespace To_do_list
                         Console.WriteLine(ToDoList[i]);
                     }
                 }
-                Console.WriteLine("Would you like to add somethine to your list?");
+                Console.WriteLine("Would you like to add something to your list?");
                 Console.WriteLine("1. Yes");
                 Console.WriteLine("2. No");
                 int choice = int.Parse(Console.ReadLine());
@@ -60,14 +66,25 @@ namespace To_do_list
             OutputtingList(ToDoList);
             RemovingTasks(ToDoList);
         }
+
         static void AddingThingsToList(List<Task> ToDoList)
         {
-            Console.WriteLine("Add something to your to do list");
-            string Task = Console.ReadLine();
-            ToDoList.Add(Task);
+            Console.WriteLine("Add something to your to-do list:");
+            string taskName = Console.ReadLine();
+            Console.WriteLine("Enter the importance out of 10:");
+            int importance = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the date it needs to be completed by (dd/mm/yyyy):");
+            string stringDate = Console.ReadLine();
+            string[] stringDates = stringDate.Split('/');
+            int date = int.Parse(stringDates[0]);
+            int month = int.Parse(stringDates[1]);
+            int year = int.Parse(stringDates[2]);
+            Task addingTask = new Task(taskName, importance, date, month, year);
+            ToDoList.Add(addingTask);
             Console.WriteLine();
         }
-        static void OutputtingList(List<string> ToDoList)
+
+        static void OutputtingList(List<Task> ToDoList)
         {
             if (ToDoList.Count == 0)
             {
@@ -81,10 +98,11 @@ namespace To_do_list
                 }
             }
         }
-        static void RemovingTasks(List<string> ToDoList)
+
+        static void RemovingTasks(List<Task> ToDoList)
         {
-            int Decision = 1;
-            while (Decision == 1)
+            int decision = 1;
+            while (decision == 1)
             {
                 if (ToDoList.Count == 0)
                 {
@@ -95,23 +113,29 @@ namespace To_do_list
                 int choice = int.Parse(Console.ReadLine());
                 choice--;
                 ToDoList.RemoveAt(choice);
-                Console.WriteLine("This is your updated list: ");
+                Console.WriteLine("This is your updated list:");
                 OutputtingList(ToDoList);
                 Console.WriteLine("Would you like to go again?");
                 Console.WriteLine("1. Yes");
                 Console.WriteLine("2. No");
-                Decision = int.Parse(Console.ReadLine());
+                decision = int.Parse(Console.ReadLine());
             }
         }
+
         static void InputtingList(List<Task> ToDoList)
         {
             StreamReader fileToRead = new StreamReader("C:/Numbers.txt");
-            string txt = fileToRead.ReadToEnd(); // read entire contents
+            int numOfLines = 0;
             while (fileToRead.ReadLine() != null)
             {
-                string[] Sections = fileToRead.ReadLine().Split(',');
-                Task temp = new Task(Sections[0], int.Parse(Sections[1]), int.Parse(Sections[2]), int.Parse(Sections[3]), int.Parse(Sections[4]));
+                string[] sections = fileToRead.ReadLine().ToString().Split(',');
+                Task temp = new Task(sections[0], int.Parse(sections[1]), int.Parse(sections[2]), int.Parse(sections[3]), int.Parse(sections[4]));
                 ToDoList.Add(temp);
+                Console.WriteLine(temp);
+            }
+            for (int i = 0; i < numOfLines; i++)
+            {
+                // Your additional logic here
             }
         }
     }
